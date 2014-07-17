9ff5207c6932c65e32b568292421cc02fbeedc26
radical-edo <radical.3do@gmail.com>
2014-07-17 06:18:16 +0200

    sometimes models needed to be required are gonna be deeper down the directory tree. definitely supporting that option

--- tests/lib/mongoose-bootstrap/models_mocha.js
+++ tests/lib/mongoose-bootstrap/models_mocha.js
@@ -9,5 +9,21 @@ describe('models', function() {
     it('has root', function() {
       assert.equal(root, 'root')
     })
+    it('has no polypore', function() {
+      assert.equal(typeof polypore, 'undefined')
+    })
+    context('branches', function() {
+      it('has a branch', function() {
+        assert.equal(branch, 'branch')
+      })
+      context('leaves', function() {
+        it('has leaf #1', function() {
+          assert.equal(leaf_1, 'leaf_1')
+        })
+        it('has leaf #2', function() {
+          assert.equal(leaf_2, 'leaf_2')
+        })
+      })
+    })
   })
 })
